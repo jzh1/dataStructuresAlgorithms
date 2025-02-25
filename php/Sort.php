7c0132a22c2521bc71a7be7e4d83b43cc2ba0a34
江兆辉 <949363409@qq..com>
2025-02-25 12:07:08 +0800

    选择排序

--- php/Sort.php
+++ php/Sort.php
@@ -6,23 +6,25 @@ require 'vendor/autoload.php';
 class Sort
 {
     // 选择排序
-    public function selectSorting(array $array)
+    public function selectSorting(&$array)
     {
         // 循环
         $maxNum = count($array);
         for ($i = 0; $i < $maxNum; $i++) {
             // 指针, 默认指针放到当前循环的位置
             $pointer = $i;
-            foreach ($array as $key => $value) {
-                dump($value);
-                // 这个大于小于是调整排序的
-                if($array[$pointer] > $value){
-                    var_dump($pointer);
-                    $pointer = $key;
+            // 一次最外层循环找到一个数，所以从第i次找，前面已经找好的跳过即可
+            for ($j = $i; $j < $maxNum; $j++) {
+                // 这个大于是倒序小于是正序调整排序的
+                // 遇到比自己小的数，把指针放过去
+                if($array[$pointer] > $array[$j]){
+                    $pointer = $j;
+                    dump('更换指针指向位置------------'.$pointer);
                 }
-                // 循环到最后一个，调换位置
-                if ($key == ($maxNum-1)){
-                    var_dump('------------------',$key);
+
+                // 循环到最后一个，根据指针，调换位置；
+                if ($j == ($maxNum-1)){
+                    dump('最后一次循环交换位置：'.$j.'------------------'.$pointer);
                     $temp = $array[$i];
                     $array[$i] = $array[$pointer];
                     $array[$pointer] = $temp;
@@ -36,5 +38,6 @@ class Sort
 }
 
 $sort = new Sort();
-$select = $sort->selectSorting([9,1,88,8,2]);
+$arr = [9,1,88,8,2];
+$select = $sort->selectSorting($arr);
 print_r($select);
